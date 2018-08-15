d8cbdcacf4a4e1be3c25614e70102a5914441f07
Mohamed Hamed Mostafa Ahmed <mohamed.hamed@itworx.com>
2018-08-15 05:33:28 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const hbs = require('hbs');
+const os = require('os');
+
+const port = 3000;
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    res.render('maintenance');
+});
+
+app.get('/',
+    (req, res) => {
+        res.render('home',
+            {
+                userName: os.userInfo().username,
+                currentYear: new Date().getFullYear()
+            });
+    });
+
+app.get('/about',
+    (req, res) => {
+        res.render('about');
+    });
+
+app.listen(port, () => console.log(`Server is up and running on port ${port}`));
+
